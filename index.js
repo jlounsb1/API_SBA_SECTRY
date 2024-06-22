30870b9826c8a47502cdbc1c88474f573a253102
jlounsb1 <jefflounsbury@gmail.com>
2024-06-22 14:49:03 -0400

    updated readme and created score functionality

--- index.js
+++ index.js
@@ -8,14 +8,19 @@ const btnOne = document.getElementById('btnone');
 const btnTwo = document.getElementById('btntwo')
 const imgOne = document.getElementById('imgone');
 const imgTwo = document.getElementById('imgtwo');
+const teamOneScore = document.getElementById('teamonescore');
+const teamTwoScore = document.getElementById('teamtwoscore');
+const winMessage = document.getElementById('winmessage');
+let teamOneCounter =0;
+let teamTwoCounter =0;
 
+
+//getting the url with saync function
 async function randomImage(space) {
     const response = await fetch("https://dog.ceo/api/breeds/image/random");
     const jsonData = await response.json()
-    const url = jsonData.message
-    console.log(url)
+    let url = jsonData.message
     space.setAttribute('src', url);
-    
 }
 
 randomImage(imgOne);
@@ -25,10 +30,32 @@ btnOne.addEventListener('click', handleClickOne);
 btnTwo.addEventListener('click', handleClickTwo);
 
 function handleClickOne() {
+    winMessage.textContent = `First team to 10 Wins!`
     randomImage(imgOne);
     randomImage(imgTwo);
+    teamOneCounter = teamOneCounter+1;
+    teamOneScore.textContent=`${teamOneCounter}`;
+    if(teamOneCounter>=10) {
+        winMessage.textContent = `Team One Wins! Try playing again!`
+        teamOneCounter=0;
+        teamTwoCounter=0;
+        teamOneScore.textContent=`${teamOneCounter}`;
+        teamTwoScore.textContent=`${teamTwoCounter}`;
+    }
+
 }
 function handleClickTwo() {
+    winMessage.textContent = `First team to 10 Wins!`
     randomImage(imgOne);
     randomImage(imgTwo);
+    teamTwoCounter = teamTwoCounter+1;
+    teamTwoScore.textContent=`${teamTwoCounter}`;
+    if(teamTwoCounter>=10) {
+        winMessage.textContent = `Team Two Wins! Try playing again!`
+        teamOneCounter=0;
+        teamTwoCounter=0;
+        teamOneScore.textContent=`${teamOneCounter}`;
+        teamTwoScore.textContent=`${teamTwoCounter}`;
+    }
 }
+
