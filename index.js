f529c6d4d8b914026ca80ad7c4eae82181e5fb8e
jlounsb1 <jefflounsbury@gmail.com>
2024-06-22 12:40:04 -0400

    displayed images by pulling pucntion twice with parameters, rounded edges

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const teamOne = document.getElementById('teamone');
 const teamTwo = document.getElementById('teamtwo');
 
 
-async function randomImage() {
+async function randomImage(space) {
     const response = await fetch("https://dog.ceo/api/breeds/image/random");
     const jsonData = await response.json()
     const url = jsonData.message
     console.log(url)
+    const img = document.createElement('img');
+    img.setAttribute('src', url);
+    space.appendChild(img)
 }
 
-randomImage()
+randomImage(teamOne);
+randomImage(teamTwo);
