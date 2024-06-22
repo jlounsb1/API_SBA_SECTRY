b3befbdffe355e671f645bb40b437aaf9549df18
jlounsb1 <jefflounsbury@gmail.com>
2024-06-22 12:30:05 -0400

    setup api pull to get random image of dog

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('connection initial test')
+//getting all the elements of my Dom
+const appContainer = document.getElementById('appcontainer');
+const header = document.getElementById('header');
+const centerPiece = document.getElementById('centerpiece');
+const teamOne = document.getElementById('teamone');
+const teamTwo = document.getElementById('teamtwo');
+
+
+async function randomImage() {
+    const response = await fetch("https://dog.ceo/api/breeds/image/random");
+    const jsonData = await response.json()
+    const url = jsonData.message
+    console.log(url)
+}
+
+randomImage()
